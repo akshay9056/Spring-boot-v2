196214e3bc22ddca8b517937f2bf5d1d988b8761
Akshay Veesam <67137623+akshay9056@users.noreply.github.com>
2026-01-28 20:07:57 +0530

    Delete src directory

--- src/main/java/com/avangrid/gui/avangrid_backend/exception/AzureBlobRepositoryException.java
+++
@@ -1,9 +0,0 @@
-package com.avangrid.gui.avangrid_backend.exception;
-
-public  class AzureBlobRepositoryException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public AzureBlobRepositoryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
